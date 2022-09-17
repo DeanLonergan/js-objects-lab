f709e347cb1c4457953389b620a8c5ca2fcf6413
Dean Lonergan <74914758+DeanLonergan@users.noreply.github.com>
2022-09-17 17:06:44 +0100

    Loop constructs.

--- fundamentals.js
+++ fundamentals.js
@@ -1,13 +1,19 @@
 const aCar = {
   owner: "Joe Bloggs",
+  previous_owners: [
+    { name: "Pat Smith", address: "1 Main Street" },
+    { name: "Sheila Dwyer", address: "2 High Street" }
+  ],
   type: {
     make: "Toyota",
     model: "Corolla",
     cc: 1.8
   },
-  mileage: 10000,
+  features : ['Parking assist', 'Alarm', 'Tow-bar'],
   registration: { year: 201, county: "WD", number: 1058 }
 };
+
+aCar.mileage = 10000;
 
 aCar.colour = {
   exterior: "red",
@@ -26,10 +32,24 @@ console.log(
 );
 
 console.log(
-  "It is a " + aCar.colour.exterior 
-    + " car with " + aCar.mileage 
-    + " miles on the clock and a " 
-    + aCar.colour.interior.colour + " "
-    + aCar.colour.interior.texture 
-    + " interior."
+  "It is a " + aCar.colour.exterior + " car with " + 
+  aCar.mileage + " miles on the clock and a " + 
+  aCar.colour.interior.colour + " " + 
+  aCar.colour.interior.texture + " interior."
 );
+
+console.log(
+  '\nFirst owner: ' + 
+  aCar.previous_owners[0].name + 
+  " - " + 
+  aCar.previous_owners[0].address)
+
+console.log("\nFeatures:");
+  for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+}
+
+console.log("\nPrevious owners: ");
+for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+  console.log(aCar.previous_owners[i].name);
+}
